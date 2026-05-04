90ed9d0d89c1a7838265834bf8e56245b4603c9d
Jamil <jamil@Jamils-MacBook-Air.local>
2026-05-04 21:37:02 +0600

    add pod file

---
+++ Pods/Target Support Files/Pods-Druti/Pods-Druti-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Druti : NSObject
+@end
+@implementation PodsDummy_Pods_Druti
+@end
---
+++ Pods/Target Support Files/Pods-SmartFonts/Pods-SmartFonts-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SmartFonts : NSObject
+@end
+@implementation PodsDummy_Pods_SmartFonts
+@end
